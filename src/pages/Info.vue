c50abf227a5af4cd56c08285de34975644c9ace8
TimeWz667 <timewz667@gmail.com>
2020-04-13 10:35:00 +0100

    Resize fonts

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,75 +1,79 @@
 <template>
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-7">
       <card>
         <template slot="header">
           <h4 class="card-title">Our team</h4>
         </template>
         <div class="content">
           <div class="typo-line">
-            <h2>
+            <h3>
               <p class="category">Research<br>Associate</p>Chu-Chang Ku
               <br>
               <small>Health Economics and Decision Science, University of Sheffield</small>
-            </h2>
+            </h3>
             <div class="row">
-              <div class="col-md-3">
+              <div class="col-md-6">
                 <h5>Email:</h5><p>c.ku@sheffield.ac.uk</p>
               </div>
-              <div class="col-md-3">
+              <div class="col-md-6">
                 <h5>Github:</h5><p><a href="https://github.com/TimeWz667" target="_blank">TimeWz667</a></p>
               </div>
-              <div class="col-md-6">
-                <h5>Study fields:</h5>
+<!--              <div class="col-md-5">
                 <ol>
                   <li>Health economics</li>
                   <li>Infectious disease modelling</li>
                   <li>Time-series analysis</li>
                 </ol>
-              </div>
+              </div>-->
             </div>
           </div>
 
           <div class="typo-line">
-            <h2>
+            <h3>
               <p class="category">Research<br>Assistant</p>Ta-Chou Ng
               <br>
               <small>Institute of Epidemiology and Preventive Medicine, National Taiwan University</small>
-            </h2>
+            </h3>
             <div class="row">
-              <div class="col-md-3">
+              <div class="col-md-6">
                 <h5>Email:</h5><p>XX@gmail.com</p>
               </div>
-              <div class="col-md-3">
+              <div class="col-md-6">
                 <h5>Github:</h5><p><a href="https://github.com/dachuwu" target="_blank">dachuwu</a></p>
               </div>
-              <div class="col-md-6">
-                <h5>Study fields:</h5>
+<!--              <div class="col-md-5">
                 <ol>
                   <li>Spatial analysis</li>
                   <li>Infectious disease modelling</li>
                   <li>Disease burden</li>
                 </ol>
-              </div>
+              </div>-->
             </div>
           </div>
 
           <div class="typo-line">
-            <h2>
+            <h3>
               <p class="category">Professor</p>Hsien-Ho Lin
               <br>
               <small>Institute of Epidemiology and Preventive Medicine, National Taiwan University</small>
-            </h2>
+            </h3>
           </div>
         </div>
       </card>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-5">
       <card>
         <template slot="header">
           <h4 class="card-title">We thank...</h4>
         </template>
         <div class="content">
+          <div class="typo-line">
+            <h5>
+              <p class="category">Epi helpers
+              </p> a, b, c </h5>
+          </div>
+
           <div class="typo-line">
             <h5>
               <p class="category"><a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">&nbsp; Creative Tim.</a>
--- src/pages/LearnEpi.vue
+++ src/pages/LearnEpi.vue
@@ -1,9 +1,27 @@
 <template>
     <div class="row">
       <b-tabs class="col-12">
+        <b-tab :title="$t('class.indices')" active>
+          <div class="row">
+            <b-nav vertical class="col-md-3">
+              <card title="Menu">
+                  <b-nav-item
+                      :id="page.title"
+                      v-for="(page, index) in epiIndices"
+                      :key="index"
+                      v-on:click="activatePage('epiIndices', page)"
+                      style="font-size: 22px; color: #0b2e13"
+                  >{{ $t(page.title) }}</b-nav-item>
+              </card>
+            </b-nav>
+            <div class="col-md-9">
+              <html-card
+                  :title="$t(activePages.epiIndices.title)"
+                  :url="activePages.epiIndices.url[$i18n.locale]"></html-card>
+            </div>
+          </div>
 
-
-
+        </b-tab>
         <b-tab title="R0 and R(t)" lazy>
           <div class="row">
             <div class="col-md-6">
@@ -18,7 +36,6 @@
                   :url="pages.Rt"></markdown-card>
             </div>
           </div>
-
         </b-tab>
         <b-tab title="Incidence and notification" lazy>
 
@@ -60,10 +77,42 @@
           SI: this.lang === "en"?
             "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_Rt_en.md":
             "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_Rt_en.md",
+        },
+        epiIndices: [
+          {
+            title: "indices.r0",
+            id: "r0",
+            url: {
+              en_UK: "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_R0_en.html",
+              zh_TW: "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_R0_tw.html"
+            }
+          },
+          {
+            title: "indices.rt",
+            id: "rt",
+            url: {
+              en_UK: "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_Rt_en.html",
+              zh_TW: "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_Rt_tw.html"
+            }
+          }
+        ],
+        activePages: {
+          epiIndices: "https://covid-19-modelling.github.io/DashboardData/Docs/Learn_R0_en.html"
         }
       };
+    },
+    mounted() {
+      this.activatePage("epiIndices", this.epiIndices[0]);
+    },
+    methods: {
+      activatePage(tab, page) {
+        this.activePages[tab] = page;
+      }
     }
   };
 </script>
 <style>
+body {
+  font-size: 20px;
+}
 </style>
